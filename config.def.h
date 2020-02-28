eedf382f16e06cea14bdd365a65fb5490b1db123
Waldomiro Seabra <wjuan.06@gmail.com>
2020-02-27 20:42:46 -0400

    ghost blue

--- config.def.h
+++ config.def.h
@@ -16,16 +16,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:style=Bold,Regular:size=10" };
 static const char dmenufont[]       = "RobotoMono Nerd Font:style=Bold:size=10";
-static const char col_gray1[]       = "#040c1a";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#181a1b";
+static const char col_gray2[]       = "#1f2d46";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#503645";
-static const char col_border_sel[]  = "#bf8a91";
+static const char col_gray4[]       = "#246b6b";
+static const char col_cyan[]        = "#0099cc";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_border_sel  },
+	[SchemeSel]  = { col_gray2, col_cyan,  col_cyan  },
 };
 
 /* tagging */
@@ -81,7 +80,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray2, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = {"brave", NULL};
 static const char *thunarcmd[] = {"thunar", NULL};
